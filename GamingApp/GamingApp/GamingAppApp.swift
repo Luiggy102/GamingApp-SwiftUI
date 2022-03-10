a31adbdcee20ddb36faf5b24d7a3ade982bd23f4
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-03-10 12:12:18 -0500

    added main xcode project

---
+++ GamingApp/GamingApp/GamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamingAppApp.swift
+//  GamingApp
+//
+//  Created by LMCM on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
