51984aa723fbb68b9d9a105e87e38258e5a992e3
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-03-10 21:00:20 -0500

    log and sign up views structures

--- GamingApp/GamingApp/ContentView.swift
+++ GamingApp/GamingApp/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Spacer()
+            // Color pide Double, se divide el # por 255
+            Color(red: 19/255, green: 30/255, blue: 53/255, opacity: 1.0).ignoresSafeArea() // Background
+            VStack {
+                Image("AppLogo").resizable().scaledToFit().frame(width: 250).padding()
+                LoginAndRegisterView()
+            }
+        }
+    }
+}
+
+struct LoginAndRegisterView: View {
+    @State var typeOfLogIn: Bool = true
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button("Log in") {
+                    typeOfLogIn = true
+                } .foregroundColor(typeOfLogIn ? .white : .gray)
+                Spacer()
+                Button("Sign up") {
+                    typeOfLogIn = false
+                } .foregroundColor(typeOfLogIn ? .gray : .white)
+                Spacer()
+            }.padding()
+            Spacer(minLength: 42)
+            if typeOfLogIn == true {
+                LogInView()
+            } else {
+                SignInView()
+            }
+        }
+    }
+}
+
+struct LogInView: View {
+    var body: some View {
+        Text("Log in view").foregroundColor(.white)
+    }
+}
+
+struct SignInView: View {
+    var body: some View {
+        Text("Sign in view").foregroundColor(.white)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("screen1").resizable()
         ContentView()
+        LoginAndRegisterView()
+        LogInView()
+        SignInView()
+        Image("screen2").resizable()
     }
 }
