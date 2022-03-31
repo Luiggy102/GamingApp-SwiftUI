5fc96116d1efe136920d6103aced0fc57c43f4e0
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-03-31 19:22:06 -0400

    added file for Codeable for Json

---
+++ GamingApp/GamingApp/Models/Model.swift
@@ -0,0 +1,28 @@
+//
+//  Model.swift
+//  GamingApp
+//
+//  Created by LMCM on 3/31/22.
+//
+
+import Foundation
+
+// Cada struct equivale a un Json {}
+struct Game: Codable {
+    let title, studio, contentRaiting, publicationYear: String
+    let welcomeDescription: String
+    let platforms, tags: [String]
+    let videosUrls: VideosUrls
+    let galleryImages: [String]
+
+//    enum CodingKeys: String, CodingKey {
+//        case title, studio, contentRaiting, publicationYear
+//        case welcomeDescription = "description"
+//        case platforms, tags, videosUrls, galleryImages
+//    }
+}
+
+// MARK: - VideosUrls
+struct VideosUrls: Codable {
+    let mobile, tablet: String
+}
